841d13dfcb269017428c2400a2829b519768e609
Bappa Barik <61709328+bappabarik@users.noreply.github.com>
2025-07-28 14:14:55 +0530

    fix: comment out logging of service worker registration for cleaner output

--- src/main.jsx
+++ src/main.jsx
@@ -128,7 +128,7 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker
       .register('/firebase-messaging-sw.js')
       .then((registration) => {
-        console.log('Service Worker registered:', registration);
+        // console.log('Service Worker registered:', registration);
       })
       .catch((error) => {
         console.log('Service Worker registration failed:', error);
